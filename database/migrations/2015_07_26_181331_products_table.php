32edf4d2fc50758accd8dd6671101b7ba3ea80ea
serovvitaly <serovvitaly@gmail.com>
2015-07-29 16:10:07 +0300

    Update 2015_07_26_181331_products_table.php
    добавлены поля в таблицу продуктов

--- database/migrations/2015_07_26_181331_products_table.php
+++ database/migrations/2015_07_26_181331_products_table.php
@@ -14,13 +14,28 @@ class ProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('user_id');
             $table->integer('catalog_id');
             $table->string('code');
             $table->string('article');
             $table->string('name');
-            $table->string('description', 3000);
+            $table->string('description', 5000);
             $table->integer('purchase_template_id');
             $table->string('source_url', 255);
+            
+            $table->string('brand', 100);
+            $table->integer('weight');
+            $table->string('measure_unit', 10);
+            $table->integer('in_stock');
+            $table->integer('min_party');
+            
+            $table->double('price_1');
+            $table->double('price_2');
+            $table->double('price_3');
+            $table->double('price_4');
+            
+            $table->integer('category_id');
+            $table->string('product_line', 100);
 
             $table->timestamps();
         });
